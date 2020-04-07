1357e59035a74934ceec6a824e4d6de9c0aaaaeb
Jianmin0105 <42919242+Jianmin0105@users.noreply.github.com>
2020-04-07 15:24:26 -0700

    import org.jason.jar dependency

---
+++ app/src/main/java/com/example/githubjob/JobItem.java
@@ -0,0 +1,38 @@
+package com.example.githubjob;
+import org.json.JSONException;
+import org.json.JSONObject;
+public class JobItem {
+    private String url;
+    private String company;
+    private String location;
+    private String title;
+    private String logo;
+    public JobItem(JSONObject obj) throws JSONException {
+
+        this.url = obj.getString("url");
+        this.company = obj.getString("company");
+        this.location = obj.getString("location");
+        this.title = obj.getString("title");
+        this.logo = obj.getString("company_logo")
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+}
